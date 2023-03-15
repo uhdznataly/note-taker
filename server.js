a45fe6fa18a52feca4c7491977b84ae61a93f91c
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-14 21:58:30 -0500

    trying to fix issues

--- server.js
+++ server.js
@@ -1,63 +1,91 @@
-//set all our dependencies
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const uuid = require('./helpers/uuid');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
+const notes = require('./db/db.json');
+
+const app = express();
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
+const readFromFile = util.promisify(fs.readFile);
 
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
 
-//GET route for homepage
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Read db.json file and return saved notes as JSON
 app.get('/api/notes', (req, res) => {
-readFile('db/db.json')
-  .then(data => {
-    res.send(data)
-  })
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// Create and save new note
 app.post('/api/notes', (req, res) => {
-const newNote = req.body;
-newNote.id = uuidv4();
-readFile('db/db.json')
-  .then(data => {
-    const allNotes = JSON.parse(data);
-    allNotes.push(newNote);
-    writeFile('db/db.json', JSON.stringify(allNotes)).then(data => {
-      res.json(newNote);
-    })
-  })
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
 });
 
-// Delete a note
 app.delete('/api/notes/:id', (req, res) => {
-readFile('db/db.json')
-.then(data => {
-  let allNotes = JSON.parse(data);
-  const { id } = req.params;
-  allNotes = allNotes.filter(note => note.id !== id);
-  writeFile('db/db.json', JSON.stringify(allNotes))
-  .then(data => {
-    res.json(allNotes);
-  })
-})
+    if (req.params.id) {
+        const id = req.params.id;
+        var indexPosition = -1;
+        for (let i = 0; i < notes.length; i++) {
+            const currentNote = notes[i];
+            if (currentNote.id === id) {
+                indexPosition = i;
+            }
+        }
+        if (indexPosition === -1) {
+            res.status(404).json('ID not found');
+        }
+        else {
+            notes.splice(indexPosition, 1);
+            writeToFile('./db/db.json', notes);
+            return res.send();
+        }
+    }
 });
-  
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
